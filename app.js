0ffc4794fdd63c0962e3e5622f8fe85cf616efc2
Tanvir Deol <tanvirdeol31@gmail.com>
2022-01-11 16:34:22 -0500

    update

--- app.js
+++ app.js
@@ -179,9 +179,12 @@ function switchTurns(){
             propertyExpanded: p1NewPropertyExpanded,
             housesBought: p1NewHouses};
         const res = axios.put('http://127.0.0.1:8000/api/interactions/5/',p1Data);
-        const p2Data = await axios.get('http://127.0.0.1:8000/api/interactions/6/')
+        console.log("Yeet");
+        const p2Data = axios.get('http://127.0.0.1:8000/api/interactions/6/')
         .then(function(response){console.log(response)});
         console.log(p2Data);
+        console.log("1");
+        
 
     }else{ //player 2
         turn = false;
